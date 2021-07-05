102d16082301ee493876e882efcb886ef25d2c52
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-07-05 22:21:02 +0300

    Bunch of fixes after first test

--- out/Avaritia BE/lib/ItemAnimHelper.js
+++ out/Avaritia BE/lib/ItemAnimHelper.js
@@ -36,11 +36,11 @@ var IAHelper;
         var anim = FileTools.ReadImage(__dir__ + "/" + srcPath + srcName + ".png");
         for (var i = 0; i < anim.getHeight() / anim.getWidth(); i++) {
             var bmp = android.graphics.Bitmap.createBitmap(anim.getWidth(), anim.getWidth(), android.graphics.Bitmap.Config.ARGB_8888);
-            var cvs = new android.graphics.Canvas(bmp);
-            var colors = [];
-            anim.getPixels(colors, 0, anim.getWidth(), 0, anim.getWidth() * i, anim.getWidth(), anim.getWidth());
-            var bmp2 = android.graphics.Bitmap.createBitmap(colors, anim.getWidth(), anim.getWidth(), android.graphics.Bitmap.Config.ARGB_8888);
-            cvs.drawBitmap(bmp2, 0, 0, null);
+            for (var x = 0; x < anim.getWidth(); x++) {
+                for (var y = 0; y < anim.getWidth(); y++) {
+                    bmp.setPixel(x, y, anim.getPixel(x, y + anim.getWidth() * i));
+                }
+            }
             FileTools.WriteImage(__dir__ + "/" + resultPath + resultName + "_" + i + ".png", bmp);
         }
     }
@@ -53,30 +53,28 @@ var IAHelper;
      * @param frames how many frames has the item texture animation
      */
     function makeCommonAnim(id, textureName, ticks, frames) {
-        var obj = IAHelper.itemAnims[textureName];
-        if (typeof obj === "undefined") {
-            obj = { meta: 0, timer: 0 };
-            Callback.addCallback("LocalTick", function () {
-                if (obj.timer + 1 == ticks) {
-                    if (obj.meta < frames)
-                        obj.meta++;
-                    else
-                        obj.meta = 0;
-                }
-                if (obj.timer < ticks)
-                    obj.timer++;
-                else
-                    obj.timer = 0;
-            });
-            Item.registerIconOverrideFunction(id, function (item, isModUi) {
-                return {
-                    name: textureName,
-                    data: IAHelper.itemAnims[textureName].meta
-                };
-            });
-        }
-        else
+        if (!!IAHelper.itemAnims[textureName])
             return Logger.Log("An error occured calling 'ItemAnimHelper.makeCommonAnim' method. Another animation is already bound to item '" + Item.getName(id, 0) + "'", "ItemAnimHelper ERROR");
+        IAHelper.itemAnims[textureName] = { meta: 0, timer: 0 };
+        var obj = IAHelper.itemAnims[textureName];
+        Callback.addCallback("LocalTick", function () {
+            if (obj.timer + 1 == ticks) {
+                if (obj.meta < frames)
+                    obj.meta++;
+                else
+                    obj.meta = 0;
+            }
+            if (obj.timer < ticks)
+                obj.timer++;
+            else
+                obj.timer = 0;
+        });
+        Item.registerIconOverrideFunction(id, function (item, isModUi) {
+            return {
+                name: textureName,
+                data: IAHelper.itemAnims[textureName].meta
+            };
+        });
     }
     IAHelper.makeCommonAnim = makeCommonAnim;
     /**
@@ -88,32 +86,30 @@ var IAHelper;
      * @param intervals set of different intervals between which will animate the texture
      */
     function makeAdvancedAnim(id, textureName, interval, frames) {
-        var obj = IAHelper.itemAnims[textureName];
-        if (typeof obj === "undefined") {
-            obj = { meta: 0, timer: 0, frameIndex: 0 };
-            Callback.addCallback("LocalTick", function () {
-                if (obj.timer + 1 == interval) {
-                    if (obj.frameIndex < frames.length)
-                        obj.frameIndex++;
-                    else
-                        obj.frameIndex = 0;
-                    obj.meta = frames[obj.frameIndex];
-                }
-                if (obj.timer < interval)
-                    obj.timer++;
-                else
-                    obj.timer = 0;
-            });
-            Item.registerIconOverrideFunction(id, function (item, imu) {
-                return {
-                    name: textureName,
-                    data: IAHelper.itemAnims[textureName].meta
-                };
-            });
-        }
-        else
+        if (!!IAHelper.itemAnims[textureName])
             return Logger.Log("An error occured calling 'IAHelper.makeAdvancedAnim' method. Another animation is already bound to item '" + Item.getName(id, 0) + "'.", "ItemAnimHelper ERROR");
+        IAHelper.itemAnims[textureName] = { meta: 0, timer: 0, frameIndex: 0 };
+        var obj = IAHelper.itemAnims[textureName];
+        Callback.addCallback("LocalTick", function () {
+            if (obj.timer + 1 == interval) {
+                if (obj.frameIndex < frames.length)
+                    obj.frameIndex++;
+                else
+                    obj.frameIndex = 0;
+                obj.meta = frames[obj.frameIndex];
+            }
+            if (obj.timer < interval)
+                obj.timer++;
+            else
+                obj.timer = 0;
+        });
+        Item.registerIconOverrideFunction(id, function (item, imu) {
+            return {
+                name: textureName,
+                data: IAHelper.itemAnims[textureName].meta
+            };
+        });
     }
     IAHelper.makeAdvancedAnim = makeAdvancedAnim;
 })(IAHelper || (IAHelper = {}));
-EXPORT("ItemAnimHelper", IAHelper);
+EXPORT("IAHelper", IAHelper);
--- source/textures.ts
+++ source/textures.ts
@@ -1,7 +1,7 @@
 (() => {
     let files = FileTools.GetListOfFiles(`${__dir__}/assets/animated_items/`, "");
     for(let i in files){
-        let fileName = files[i].getName().replace(/[.][^.]+$/g, "");
+        let fileName = new java.lang.String(files[i].getName()).replaceFirst("[.][^.]+$", "");
         IAHelper.convertTexture("assets/animated_items/", fileName, "assets/items-opaque/", fileName);
     }
 })();
