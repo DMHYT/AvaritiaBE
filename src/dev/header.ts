39d4756d1764f6e2a117ff40310a703c3cb4e9ab
vsdum <danmarkhunter13072006@gmail.com>
2021-11-13 17:54:08 +0200

    Changed config getters and Java classes to wrap

--- src/dev/header.ts
+++ src/dev/header.ts
@@ -27,18 +27,17 @@ type UIElement = com.zhekasmirnov.innercore.api.mod.ui.elements.UIElement;
 type IWindow = com.zhekasmirnov.innercore.api.mod.ui.window.IWindow;
 
 const debug_enabled = __config__.getBool("debug");
-const MAX_GAPING_VOID_VIEW_DISTANCE = __config__.getNumber("max_gaping_void_view_distance").intValue();
-const VOID_PARTICLES_PER_TICK = __config__.getNumber("void_particles_per_tick").intValue();
-const COLLECTOR_PROCESS_TIME = __config__.getNumber("collector_process_time").intValue();
+const MAX_GAPING_VOID_VIEW_DISTANCE = __config__.getInteger("max_gaping_void_view_distance");
+const VOID_PARTICLES_PER_TICK = __config__.getInteger("void_particles_per_tick");
+const COLLECTOR_PROCESS_TIME = __config__.getInteger("collector_process_time");
 const BORING_FOOD = __config__.getBool("boring_food");
-const EYE_COLOR_UPDATE_FREQUENCY = __config__.getNumber("eye_color_update_frequency").intValue();
-const PLAYER_FLYING_CHECK_INTERVAL = __config__.getNumber("player_flying_check_for_wings_interval").intValue()
+const EYE_COLOR_UPDATE_FREQUENCY = __config__.getInteger("eye_color_update_frequency");
 
 const rand = new java.util.Random();
 
 const AVA_STUFF: number[] = [];
 
-const EntityArrow = WRAP_JAVA("ua.vsdum.avaritia.NativeArrow");
-const AvaritiaFuncs = WRAP_JAVA("ua.vsdum.avaritia.Avaritia");
+const EntityArrow = WRAP_JAVA("vsdum.avaritia.NativeArrow");
+const AvaritiaFuncs = WRAP_JAVA("vsdum.avaritia.Avaritia");
 
 const INFINITY_ITEM_FRAMES = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 7, 6, 5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1];
