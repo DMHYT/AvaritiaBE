1c438c8bc628be23c789f94ae6b1ff9710e1b9a6
vsdum <danmarkhunter13072006@gmail.com>
2021-11-13 23:32:31 +0200

    Some comments in leggings script

--- src/dev/items/armor/leggings.ts
+++ src/dev/items/armor/leggings.ts
@@ -1,3 +1,8 @@
+/**
+ * Infinity Leggings give the player full fire resistance.
+ * This script is so short because the fire resistance
+ * feature is fully made in native.
+ */
 IDRegistry.genItemID("infinity_leggings");
 Item.createArmorItem("infinity_leggings", "item.avaritia:infinity_pants.name", {name: "infinity_leggings", meta: 0}, {type: "leggings", armor: 12, durability: 9999, texture: "armor/infinity_1.png"}).setEnchantability(EEnchantType.LEGGINGS, 1000);
 AVA_STUFF.push(ItemID.infinity_leggings);
